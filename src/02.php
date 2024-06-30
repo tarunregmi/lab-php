38906ac1c3a1d0f3add23854ecc310ee595d44af
Tarun Regmi <tarunregmi.io@gmail.com>
2024-06-30 17:00:54 +0545

    logic to determine given number is even or odd

---
+++ src/02.php
@@ -0,0 +1,17 @@
+<?php
+  echo "<pre>";
+  echo "Lab - 02<br>";
+  echo "PHP program to print Even or odd number:<br><br>";
+
+
+  $number = 20;
+  
+  if ($number%2) {
+    echo "$number is Odd number";
+  } else {
+    echo "$number is Even number";
+  }
+
+
+  echo "</pre>";
+?>
