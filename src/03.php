5437f8060ce7be5b6d312a407e1ae362484a0bce
Tarun Regmi <tarunregmi.io@gmail.com>
2024-06-30 17:01:28 +0545

    logic to determine given number is prime or not

---
+++ src/03.php
@@ -0,0 +1,27 @@
+<?php
+  echo "<pre>";
+  echo "Lab - 02<br>";
+  echo "PHP program to check prime number:<br><br>";
+
+
+  $number = 67;
+  $number = abs($number);
+
+  if ($number < 2) {
+    echo "Number must be >= 2";
+  } else {
+    for ($i=2; $i<$number; $i++) {
+      if (($number % $i) === 0) break;
+    }
+    
+    if ($i === $number) {
+      echo "$number is prime number.";
+    } else {
+      echo "$number is not prime number";
+    }
+  }
+  
+
+
+  echo "</pre>";
+?>
