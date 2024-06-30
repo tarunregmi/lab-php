5fcb520818ed640a9accfb7b10c390e0e5a10ea4
Tarun Regmi <tarunregmi.io@gmail.com>
2024-06-30 17:00:09 +0545

    logic to get sum of digits

---
+++ src/01.php
@@ -0,0 +1,21 @@
+<?php
+  echo "<pre>";
+  echo "Lab - 01<br>";
+  echo "PHP program to print sum of digits:<br><br>";
+
+
+  $number = 437;
+  $digits = abs($number);
+  $sum = 0;
+
+  echo "Number: $number<br>";
+  while ($digits > 0) {
+    $reminder = $digits % 10;
+    $sum += $reminder;
+    $digits = (int)($digits / 10);
+  }
+  echo "Sum of digits: $sum";
+
+
+  echo "</pre>";
+?>
