df4693526dc1a7785f096ddf77c4b472f948ec49
Tarun Regmi <tarunregmi.io@gmail.com>
2024-07-13 17:20:58 +0545

    logic to find factorial of a number

---
+++ src/04.php
@@ -0,0 +1,18 @@
+<?php
+  echo "<pre>";
+  echo "Lab - 04<br>";
+  echo "PHP program to print factorial of a number:<br><br>";
+
+
+  $number = 7;
+
+  function factorial($n) {
+    if ($n <= 1) return 1;
+    else return ($n * factorial($n - 1));
+  }
+
+  echo "Factorial of $number is " . factorial($number) . ".";
+
+
+  echo "</pre>";
+?>
