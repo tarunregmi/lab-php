93d8f7e1080b570e9225c5a504a8ba47854e4cda
Tarun Regmi <tarunregmi.io@gmail.com>
2024-07-13 18:43:54 +0545

    logic to check palindrome number

---
+++ src/06.php
@@ -0,0 +1,24 @@
+<?php
+  echo "<pre>";
+  echo "Lab - 06<br>";
+  echo "PHP program to check palindrome number:<br><br>";
+
+
+  $number = 12321;
+  $number = (string)$number;
+  $numberLen = strlen($number);
+  $count = (int)($numberLen / 2);
+
+  for ($i=0; $i<=$count; $i++) {
+    if ($number[$i] !== $number[$numberLen - $i -1]) break;
+  }
+
+  if (($i-1) !== $count) {
+    echo "$number is not palindrome number.";
+  } else {
+    echo "$number is palindrome number.";
+  }
+
+
+  echo "</pre>";
+?>
