b0fcca804f899339e7757346d49a4b8d93c8097e
Tarun Regmi <tarunregmi.io@gmail.com>
2024-07-13 17:50:57 +0545

    logic to check is a number is Armstrong number or not

---
+++ src/05.php
@@ -0,0 +1,36 @@
+<?php
+  echo "<pre>";
+  echo "Lab - 05<br>";
+  echo "PHP program to check Armstrong number:<br><br>";
+
+
+  $number = 153;
+
+  function isArmstrong($n) {
+    global $number;
+    $sum = 0;
+
+    while($n > 0) {
+      $digit = $n % 10;
+      $sum += ($digit * $digit * $digit);
+      $n = (int)($n / 10);
+    }
+
+    if ($number !== $sum) {
+      echo "$number is not a Armstrong number.";
+    } else {
+      echo "$number is a Armstrong number.";
+    }
+  }
+
+  isArmstrong($number);
+  echo "<br>";
+  $number = 370;
+  isArmstrong($number);
+  echo "<br>";
+  $number = 120;
+  isArmstrong($number);
+
+
+  echo "</pre>";
+?>
