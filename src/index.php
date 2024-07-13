2078c892efa7e681d0eefaa6aeca83e51935c26a
Tarun Regmi <tarunregmi.io@gmail.com>
2024-07-13 17:21:14 +0545

    add questions

--- src/index.php
+++ src/index.php
@@ -7,5 +7,24 @@
 </head>
 <body>
   <pre>Lab PHP</pre>
+  <ol>
+    <li><a href="./01.php">PHP program to print sum of digits.</a></li>
+    <li><a href="./02.php">PHP program to print Even or odd number</a></li>
+    <li><a href="./03.php">PHP program to check prime number.</a></li>
+    <li><a href="./04.php">PHP program to print factorial of a number.</a></li>
+    <li><a href="./05.php">PHP program to check Armstrong number.</a></li>
+    <li><a href="./06.php">PHP program to check palindrome number.</a></li>
+    <li><a href="./07.php">PHP program to print Fibonacci series without using recursion and using recursion.</a></li>
+    <li><a href="./08.php">PHP program to reverse given number.</a></li>
+    <li><a href="./09.php">PHP functions to write your information.</a></li>
+    <li><a href="./10.php">PHP funciton to find cube of given number</a></li>
+    <li><a href="./11.php">PHP program to show passing argument and returning value in php</a></li>
+    <li><a href="./12.php">PHP program to reverse the string</a></li>
+    <li><a href="./13.php">PHP program to count the words in the string</a></li>
+    <li><a href="./14.php">PHP program remove specific element by value from an array?</a></li>
+    <li><a href="./15.php">Create a class named “Bird”. Also, create two objects pigeon and crow. Display their colors, habitats and what they eat.</a></li>
+    <li><a href="./16.php">Give an example of parameterized constructor in PHP.</a></li>
+    <li><a href="./17.php">Write a PHP program in which user inputs form data, calculate simple interest and display it in textbox</a></li>
+  </ol>
 </body>
 </html>
