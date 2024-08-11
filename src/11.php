f9d67ef0b49a5f197ad9c5839833589dcf4ab17f
Tarun Regmi <tarunregmi.io@gmail.com>
2024-08-11 19:37:12 +0545

    php program to ...

---
+++ src/11.php
@@ -0,0 +1,29 @@
+<?php
+  echo "<pre>";
+  echo "Lab - 10<br>";
+  echo "PHP program to show passing argument and returning value in php:<br><br>";
+
+
+  function calculateArea($width, $height) {
+    return $width * $height;
+  }
+
+  function getGreeting($name, $age) {
+    return "Hello, $name! You are $age years old.";
+  }
+
+  $width = 7.5;
+  $height = 10;
+  $area = calculateArea($width, $height);
+
+  $name = "Alice";
+  $age = 30;
+  $greeting = getGreeting($name, $age);
+
+  echo "The area of the rectangle is $area square units.\n";
+  echo $greeting;
+
+
+
+  echo "</pre>";
+?>
