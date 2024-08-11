5abf009ab0e593cf876975802296a0161c0bc813
Tarun Regmi <tarunregmi.io@gmail.com>
2024-08-11 19:08:29 +0545

    program to print fibonacci series with and without recursion.

---
+++ src/07.php
@@ -0,0 +1,46 @@
+<?php
+  echo "<pre>";
+  echo "Lab - 07<br>";
+  echo "PHP program to print fibonacci series without using recursion and using recursion:<br><br>";
+
+
+  $number = 10;
+
+  // without recursion
+  function fibonacciWithoutRecursion($n) {
+    $a = 0;
+    $b = 1;
+
+    for ($i = 0; $i < $n; $i++) {
+      echo $a . ", ";
+      $next = $a + $b;
+      $a = $b;
+      $b = $next;
+    }
+  }
+
+  // with recursion
+  function fibonacci($n) {
+    if ($n <= 1) {
+      return $n;
+    } else {
+      return fibonacci($n -1) + fibonacci($n - 2);
+    }
+  }
+
+  function fibonacciWithRecursion($n) {
+    for ($i = 0; $i < $n; $i++) {
+      echo fibonacci($i) . ", ";
+    }
+  }
+
+  echo "Fibonacci series up to $number terms:\n\n";
+  echo "Without recursion:\n";
+  fibonacciWithoutRecursion($number);
+  echo "\n\nWith recursion:\n";
+  fibonacciWithRecursion($number);
+  
+
+
+  echo "</pre>";
+?>
