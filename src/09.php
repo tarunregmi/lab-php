bef2e822d406b29d3581bd68ed4ba60c47d01326
Tarun Regmi <tarunregmi.io@gmail.com>
2024-08-11 19:25:22 +0545

    program to print information

---
+++ src/09.php
@@ -0,0 +1,37 @@
+<?php
+  echo "<pre>";
+  echo "Lab - 09<br>";
+  echo "PHP functions to write your information:<br><br>";
+
+
+  function displayName($firstName, $lastName) {
+    echo "Name: " . $firstName . " " . $lastName . "\n";
+  }
+
+  function displayAge($age) {
+    echo "Age: " . $age . "\n";
+  }
+
+  function displayAddress($street, $city, $state, $zip) {
+    echo "Address: " . $street . ", " . $city . ", " . $state . " " . $zip . "\n";
+  }
+
+  function displayPersonInfo($firstName, $lastName, $age, $street, $city, $state, $zip) {
+    displayName($firstName, $lastName);
+    displayAge($age);
+    displayAddress($street, $city, $state, $zip);
+  }
+
+  $firstName = "Kush";
+  $lastName = "Nepal";
+  $age = 22;
+  $street = "Araniko Highway";
+  $city = "Lokanthali";
+  $province = "Bagmati";
+  $zip = "44600";
+
+  displayPersonInfo($firstName, $lastName, $age, $street, $city, $province, $zip);
+
+
+  echo "</pre>";
+?>
