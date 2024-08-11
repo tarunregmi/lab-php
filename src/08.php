51b1c6c3acd896318fbdeed6188f4982e68af731
Tarun Regmi <tarunregmi.io@gmail.com>
2024-08-11 19:15:31 +0545

    program to reverse number

---
+++ src/08.php
@@ -0,0 +1,23 @@
+<?php
+  echo "<pre>";
+  echo "Lab - 08<br>";
+  echo "PHP program to reverse given number:<br><br>";
+
+
+  $number = 8972;
+  
+  function reverseNumber($n) {
+    $nStr = (string)$n;
+
+    $reversedStr = strrev($nStr);
+    $reversedNum = (int)$reversedStr;
+
+    return $reversedNum;
+  }
+
+  echo "Original number: $number\n";
+  echo "Reversed number: " . reverseNumber($number);
+
+
+  echo "</pre>";
+?>
