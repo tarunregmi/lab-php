db8089df0b63d0bd43dd5679f7d4ae3663c5f8e3
Tarun Regmi <tarunregmi.io@gmail.com>
2024-08-11 19:48:52 +0545

    php program to count words in the string

---
+++ src/13.php
@@ -0,0 +1,25 @@
+<?php
+  echo "<pre>";
+  echo "Lab - 13<br>";
+  echo "PHP program to count the words in the string:<br><br>";
+
+
+  function countWords($string) {
+    $words = explode(' ', trim($string));
+
+    $words = array_filter($words, function($word) {
+      return !empty($word);
+    });
+
+    return count($words);
+  }
+
+  $inputString = "Hello, world! This is a test string.";
+  $wordCount = countWords($inputString);
+
+  echo "Original string: $inputString\n";
+  echo "The number of words in the string is: $wordCount";
+
+
+  echo "</pre>";
+?>
