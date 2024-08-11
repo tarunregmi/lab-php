ca70c9d0d73b94029e3588c9e5cd13b070b4101c
Tarun Regmi <tarunregmi.io@gmail.com>
2024-08-11 19:53:26 +0545

    php program to remove specific element by value from an array

---
+++ src/14.php
@@ -0,0 +1,26 @@
+<?php
+  echo "<pre>";
+  echo "Lab - 13<br>";
+  echo "PHP program remove specific element by value from an array:<br><br>";
+
+
+  function removeElementByValue(&$array, $value) {
+    foreach($array as $key => $item) {
+      if ($item === $value) {
+        unset($array[$key]);
+      }
+    }
+
+    $array = array_values($array);
+  }
+
+  $originalArray = [1, 2, 3, 4, 2, 5];
+  $valueToRemove = 2;
+
+  removeElementByValue($originalArray, $valueToRemove);
+
+  echo "Array after removing value $valueToRemove: " . implode(", ", $originalArray);
+
+
+  echo "</pre>";
+?>
